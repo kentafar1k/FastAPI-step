f711b7ae04ba6be36fb4b9e5920155e8ecf057c2
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-05-03 14:38:40 +0300

    урок 2.2

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
+from .models import User, Feedback
 
 app = FastAPI()
 
 # запуск uvicorn app.main:app --reload --port 1111
+
+class UserOutput(User):
+    is_adult: bool = False
+
+feedbacks = []
 
 @app.get("/")
 def read_root():
@@ -16,3 +22,14 @@ def calculate(num1: int = Form(), num2: int = Form()):
 @app.get("/calculate", response_class=FileResponse)
 def calc_form():
     return "app/public/calculate.html"
+
+@app.post("/user", response_model=UserOutput)
+def get_user(user: UserOutput = UserOutput(name="andy", age=24)):
+    if user.age >= 18:
+        user.is_adult = True
+    return user
+
+@app.post("/feedback", response_model=Feedback)
+def get_feedback(feedback: Feedback):
+    feedbacks.append(feedback)
+    return {"message": f"Feedback received. Thank you, {feedback.name}."}
---
+++ app/models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    name: str
+    age: int
+
+class Feedback(BaseModel):
+    name: str
+    message: str
